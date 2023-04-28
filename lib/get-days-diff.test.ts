02395afc1cf9e3cc4d98237a5903feed49056c37
mmyoji <mmyoji@gmail.com>
2023-04-28 23:33:55 +0900

    test: tweak description

--- lib/get-days-diff.test.ts
+++ lib/get-days-diff.test.ts
@@ -6,30 +6,30 @@ import { getDaysDiff } from "@/lib/get-days-diff.ts";
   {
     base: new Date("2023-04-20T10:00:00+0900"),
     target: new Date("2023-04-27T10:00:00+0900"),
-    expected: 7,
+    days: 7,
   },
   {
     base: new Date("2023-04-20T10:00:00+0900"),
-    target: new Date("2023-04-26T10:01:00+0900"),
-    expected: 7,
+    target: new Date("2023-04-26T10:00:01+0900"),
+    days: 7,
   },
   {
     base: new Date("2023-04-20T10:00:00+0900"),
     target: new Date("2023-04-26T10:00:00+0900"),
-    expected: 6,
+    days: 6,
   },
   {
     base: new Date("2023-04-27T10:00:00+0900"),
     target: new Date("2023-04-20T10:00:00+0900"),
-    expected: 7,
+    days: 7,
   },
   {
     base: new Date("2023-04-20T10:00:00+0900"),
     target: new Date("2023-04-27T11:00:00+0900"),
-    expected: 8,
+    days: 8,
   },
-].forEach(({ base, target, expected }) => {
-  Deno.test(`getDaysDiff() returns date diff (expected=${expected}) when base=${base} target=${target}`, () => {
-    assertEquals(getDaysDiff(base, target), expected);
+].forEach(({ base, target, days }) => {
+  Deno.test(`getDaysDiff() returns ${days} (days) between ${base.toISOString()} and ${target.toISOString()}`, () => {
+    assertEquals(getDaysDiff(base, target), days);
   });
 });
