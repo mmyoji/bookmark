e5a18e1e547d4ae91c986fa794d319d585644307
mmyoji <mmyoji@gmail.com>
2023-05-04 00:50:25 +0900

    refactor: simplify query param name

--- routes/api/search-items.ts
+++ routes/api/search-items.ts
@@ -6,16 +6,16 @@ import { ItemsRepository } from "@/lib/items.repository.ts";
 
 export const handler: Handlers = {
   async GET(req) {
-    const createdAt = new URL(req.url).searchParams.get("created_at");
+    const date = new URL(req.url).searchParams.get("date");
 
-    assert(typeof createdAt === "string");
+    assert(typeof date === "string");
 
     const headers = new Headers();
     headers.set("Content-type", "application/json");
 
     const kv = await Deno.openKv();
     const repo = new ItemsRepository(kv);
-    const items = await repo.search(createdAt);
+    const items = await repo.search(date);
 
     await kv.close();
 
