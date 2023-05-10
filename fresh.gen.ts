ae29650c899deae98089b6960115286f1decc600
mmyoji <mmyoji@gmail.com>
2023-05-10 19:55:48 +0900

    feat: prepare for new login

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,19 +5,21 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/api/items.ts";
 import * as $1 from "./routes/api/login.ts";
-import * as $2 from "./routes/api/logout.ts";
-import * as $3 from "./routes/api/search-items.ts";
-import * as $4 from "./routes/index.tsx";
-import * as $5 from "./routes/login.tsx";
+import * as $2 from "./routes/api/logins.ts";
+import * as $3 from "./routes/api/logout.ts";
+import * as $4 from "./routes/api/search-items.ts";
+import * as $5 from "./routes/index.tsx";
+import * as $6 from "./routes/login.tsx";
 
 const manifest = {
   routes: {
     "./routes/api/items.ts": $0,
     "./routes/api/login.ts": $1,
-    "./routes/api/logout.ts": $2,
-    "./routes/api/search-items.ts": $3,
-    "./routes/index.tsx": $4,
-    "./routes/login.tsx": $5,
+    "./routes/api/logins.ts": $2,
+    "./routes/api/logout.ts": $3,
+    "./routes/api/search-items.ts": $4,
+    "./routes/index.tsx": $5,
+    "./routes/login.tsx": $6,
   },
   islands: {},
   baseUrl: import.meta.url,
---
+++ lib/logins.repository.test.ts
@@ -0,0 +1,45 @@
+import { assertEquals } from "std/testing/asserts.ts";
+
+import { createLogin, findLogin } from "@/lib/logins.repository.ts";
+import { initTestKV, KV } from "@/lib/kv.ts";
+
+function testDB(
+  desc: string,
+  fn: (kv: KV) => Promise<void>,
+): void {
+  Deno.test(`LoginsRepository.${desc}`, async () => {
+    const kv = await initTestKV();
+    await fn(kv);
+
+    for await (const entry of kv.list({ prefix: ["logins"] })) {
+      await kv.delete(entry.key);
+    }
+    await kv.close();
+  });
+}
+
+testDB("createLogin() saves data", async (kv) => {
+  const data = {
+    username: crypto.randomUUID(),
+    encryptedPassword: "password",
+  };
+  await createLogin(data)(kv);
+
+  const res = await kv.get<typeof data>(["logins", data.username]);
+
+  assertEquals(res.value, data);
+});
+
+testDB("findLogin() fetches by username", async (kv) => {
+  const data = {
+    username: crypto.randomUUID(),
+    encryptedPassword: "password",
+  };
+  await createLogin(data)(kv);
+
+  let login = await findLogin(data.username)(kv);
+  assertEquals(login, data);
+
+  login = await findLogin(crypto.randomUUID())(kv);
+  assertEquals(login, null);
+});
