8c81a9cc4e6d21e7fc60c81c28d5d2faeb127a97
mmyoji <mmyoji@gmail.com>
2023-06-02 19:09:24 +0900

    feat: omit redundant submit button

--- components/CreateForm.tsx
+++ components/CreateForm.tsx
@@ -1,20 +1,17 @@
-import { Button } from "@/components/Button.tsx";
 import { Input } from "@/components/Input.tsx";
 
 export function CreateForm() {
   return (
     <form method="POST" class="space-y-4" action="/api/items">
-      <div class="grid grid-cols-3 gap-x-0.5">
-        <Input
-          placeholder="URL"
-          name="url"
-          type="url"
-          required
-          class="col-span-2"
-          pattern="https?://.*"
-        />
-        <Button type="submit" class="shadow-md rounded-sm">Submit</Button>
-      </div>
+      <Input
+        placeholder="URL"
+        name="url"
+        type="url"
+        required
+        class="w-full"
+        pattern="https?://.*"
+      />
+      <input type="submit" hidden />
     </form>
   );
 }
