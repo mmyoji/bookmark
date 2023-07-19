4dc4820869b90555d4738eb5b1bfaa457c72ccc8
mmyoji <mmyoji@gmail.com>
2023-07-19 17:17:57 +0900

    refactor: utilize async route component

--- main.ts
+++ main.ts
@@ -11,6 +11,7 @@ import manifest from "./fresh.gen.ts";
 import twindPlugin from "$fresh/plugins/twindv1.ts";
 import twindConfig from "./twind.config.ts";
 
+// @ts-expect-error Async Router Components with State type is not supported yet
 await start(manifest, {
   port: Number(Deno.env.get("PORT") || "8000"),
   plugins: [twindPlugin(twindConfig)],
