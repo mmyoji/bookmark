4f4f9f4a2ba7616cdf34e94a0e3511e41654172f
mmyoji <mmyoji@gmail.com>
2023-08-27 07:39:57 +0900

    refactor: prefer std

--- lib/date.utils.ts
+++
@@ -1,4 +0,0 @@
-export function countDaysBetween(base: Date, target: Date): number {
-  const diff = Math.abs(base.getTime() - target.getTime());
-  return Math.ceil(diff / (1000 * 60 * 60 * 24));
-}
