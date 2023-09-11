709a84deb695e368f98a91bc36f290085fef8fd4
mmyoji <mmyoji@gmail.com>
2023-09-11 21:55:32 +0900

    chore: bump fresh to v1.4.3
    see https://github.com/denoland/fresh/blob/1.4.3/www/main.ts

--- main.ts
+++ main.ts
@@ -6,11 +6,8 @@
 /// <reference lib="deno.unstable" />
 
 import { start } from "$fresh/server.ts";
+
 import manifest from "./fresh.gen.ts";
+import config from "./fresh.config.ts";
 
-import twindPlugin from "$fresh/plugins/twindv1.ts";
-import twindConfig from "./twind.config.ts";
-
-await start(manifest, {
-  plugins: [twindPlugin(twindConfig)],
-});
+await start(manifest, config);
