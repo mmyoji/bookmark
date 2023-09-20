4eeff7bc273408d0e03824e3983ea7d8393d2caf
mmyoji <mmyoji@gmail.com>
2023-09-20 22:34:25 +0900

    refactor: decrease deps from routes

---
+++ lib/services/item-creation.ts
@@ -0,0 +1,25 @@
+import { assert } from "$std/assert/mod.ts";
+
+import { createItem } from "@/lib/kv/items.ts";
+import { parseTitle } from "@/lib/html-parser.ts";
+
+async function fetchTitle(url: string): Promise<string> {
+  const res = await fetch(url).catch(() => undefined);
+  if (!res || !res.ok) {
+    return "";
+  }
+
+  const text = await res.text().catch(() => "");
+  return parseTitle(text);
+}
+
+async function run(url: FormDataEntryValue | null): Promise<void> {
+  assert(typeof url === "string");
+
+  const title = await fetchTitle(url);
+  await createItem({ date: new Date(), url, title });
+}
+
+export const itemCreationService = {
+  run,
+};
