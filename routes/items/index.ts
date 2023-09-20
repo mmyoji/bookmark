306a836790a9eb62c6b15942bcea7e1b61fb574e
mmyoji <mmyoji@gmail.com>
2023-09-20 22:34:25 +0900

    refactor: decrease deps from routes

--- routes/items/index.ts
+++ routes/items/index.ts
@@ -1,20 +1,8 @@
 import { type Handlers } from "$fresh/server.ts";
-import { assert } from "$std/assert/mod.ts";
 
 import { type State } from "@/lib/context.ts";
-import { createItem } from "@/lib/kv/items.ts";
-import { parseTitle } from "@/lib/html-parser.ts";
 import { redirect } from "@/lib/response.utils.ts";
-
-async function fetchTitle(url: string): Promise<string> {
-  const res = await fetch(url).catch(() => undefined);
-  if (!res || !res.ok) {
-    return "";
-  }
-
-  const text = await res.text().catch(() => "");
-  return parseTitle(text);
-}
+import { itemCreationService } from "@/lib/services/item-creation.ts";
 
 export const handler: Handlers<unknown, State> = {
   async POST(req, ctx) {
@@ -23,12 +11,7 @@ export const handler: Handlers<unknown, State> = {
     }
 
     const form = await req.formData();
-    const url = form.get("url");
-
-    assert(typeof url === "string");
-
-    const title = await fetchTitle(url);
-    await createItem({ date: new Date(), url, title });
+    await itemCreationService.run(form.get("url"));
 
     return redirect({ location: "/" });
   },
