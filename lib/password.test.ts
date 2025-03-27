d8bbbfadcd33fa911e921efa5d3da26700054ade
mmyoji <mmyoji@gmail.com>
2025-03-27 17:48:22 +0900

    chore: suppress Code Spell Checker warnings

--- lib/password.test.ts
+++ lib/password.test.ts
@@ -2,18 +2,18 @@ import { assertEquals } from "@std/assert";
 
 import { hashPassword, verifyPassword } from "@/lib/password.ts";
 
-const pswd = "P@s$w0rd";
+const pass = "P@s$w0rd";
 
 Deno.test("verifyPassword() returns true when password matches", () => {
-  const hashed = hashPassword(pswd);
+  const hashed = hashPassword(pass);
 
-  const result = verifyPassword(pswd, hashed);
+  const result = verifyPassword(pass, hashed);
 
   assertEquals(result, true);
 });
 
-Deno.test("verifyPassword() returns false when password unmatches", () => {
-  const hashed = hashPassword(pswd);
+Deno.test("verifyPassword() returns false when password doesn't match", () => {
+  const hashed = hashPassword(pass);
 
   const result = verifyPassword("P@ssw0rd", hashed);
 
